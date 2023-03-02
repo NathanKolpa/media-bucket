22f3edc1aa6be21f84b4d09746e90724f0f27f3e
Nathan Kolpa <nathan@kolpa.me>
2023-03-02 22:26:10 +0100

    Add the Created field to the post info sidebar

---
+++ webclient/src/app/shared/pipes/ago.pipe.ts
@@ -0,0 +1,40 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ago',
+  pure: true
+})
+export class AgoPipe implements PipeTransform {
+
+  transform(value?: Date, ...args: unknown[]): string | null {
+    if (!value) {
+      return null;
+    }
+
+    const seconds = Math.floor((new Date().getTime() - value.getTime()) / 1000);
+
+    let interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+    }
+
+    return Math.floor(seconds) + " seconds ago";
+  }
+}
--- webclient/src/app/shared/pipes/pipes.module.ts
+++ webclient/src/app/shared/pipes/pipes.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DurationPipe} from "@shared/pipes/duration.pipe";
 import { BytesPipe } from './bytes.pipe';
+import { AgoPipe } from './ago.pipe';
 
 
 
 @NgModule({
   declarations: [
     DurationPipe,
-    BytesPipe
+    BytesPipe,
+    AgoPipe
   ],
   exports: [
     DurationPipe,
-    BytesPipe
+    BytesPipe,
+    AgoPipe
   ],
   imports: [
     CommonModule
